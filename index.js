6d1f5af2a194881fbdd0cd9f94e85777d836996d
Dejenu Feleke <djfeleke@gmail.com>
2021-05-13 11:28:59 -0400

    Starting on JS

---
+++ index.js
@@ -0,0 +1,13 @@
+let validFormFieldInput = (data) => {
+    const taskname = document.querySelector('#taskname');
+    const taskName = taskname.value;
+    if(taskName = ''){
+        
+    }
+    console.log("Task Name:  "+taskName);
+
+}
+// Add event listener to the form to run valide form input
+// If the input is not valide add the 'show' alert class to the alert element
+
+
